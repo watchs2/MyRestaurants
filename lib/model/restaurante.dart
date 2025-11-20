90c0c7c83b90cb52c68de15429e15bc6a4116160
Utilizador <ruben.agostinho11@gmail.com>
2025-11-20 00:20:58 +0000

    startProject

---
+++ lib/model/restaurante.dart
@@ -0,0 +1,51 @@
+class Restaurant {
+  var name;
+  var adress;
+  var phone;
+  var latitude;
+  var longitude;
+  var imgid;
+
+  //ns se fica aqui
+  var stars;
+}
+
+
+/*
+Abro a app e tem varios restaurantes ou seja uma lista de restaurantes(com filtros)
+
+se clico no restaurante 
+  vejo o nome adress phone e no mapa e imagem se tiver
+  depois posso dar estrelas fazer uma review
+  posso editar as informações
+  e eliminar
+
+
+Telas:
+
+1-> Lista
+    lista de restaurantes adicionados
+    com filtros(distance nome etc)
+
+2-> About
+  posso então editar
+  eliminar
+  e dar votação
+
+3-> Criar Restaurante
+  pede:
+   name;
+   adress;
+   phone;
+
+  usar localização(latitude e longitude)   
+   imgid;
+
+
+
+Components
+cada component que mostra pequena info
+
+
+
+*/
